c4f3b96a3972bea0219199ac2617ce379dc1e74a
Bart Barszczewski <154055563+bartek-barszczewski@users.noreply.github.com>
2024-03-10 11:02:34 +0100

    Create nodejs-yt branch, init direcotory

--- nodejs-udemy/controllers/error/error.js
+++
@@ -1,3 +0,0 @@
-module.exports.errorRedirect = (req, res, next) => {
-    res.redirect("/");
-};
--- nodejs-udemy/routes/error/error.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const errorController = require("./../../controllers/error/error.js");
-
-const errorRoutes = express.Router();
-
-errorRoutes.get("*", errorController.errorRedirect);
-
-module.exports = errorRoutes;
